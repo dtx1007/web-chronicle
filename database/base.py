a279eb974cd2597220d4f41222a4dd2af8067aee
lod1004 <lod1004@alu.ubu.es>
2025-01-06 21:27:15 +0100

    Cambiada la inicialización de la db

--- database/base.py
+++ database/base.py
@@ -1,9 +1,17 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
 
-
 db: SQLAlchemy = SQLAlchemy(model_class=Base)
+
+def initialize_database():
+    """
+    Inicializa la base de datos creando todas las tablas definidas en los modelos.
+    """
+    engine = create_engine('sqlite:///:memory:') 
+    Base.metadata.create_all(bind=engine)
+    db.session = scoped_session(sessionmaker(bind=engine))
